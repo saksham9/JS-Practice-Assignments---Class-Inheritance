33db97301267106456db77746a0986e87d531981
saksham9 <agrawal.saksham9@gmail.com>
2020-10-24 00:14:55 +0530

    submit

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,17 @@
 // import using require
+const Shape=require(Shape);
 
 // declare class
+class Circle extends Shape{
+    constructor(props){
+        super(props);
+    }
+    calculateArea(){
+        return "Circle Area";
+    }
+}
 
 
 // export class using module.exports
+module.exports=Circle;
+
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
-
+class Shape{
+    constructor(props) {
+        super(props);
+        this.color="red";
+    }
+    drawShape(){
+        return "Shape";
+    }
+    calculateArea(){
+        return "Area";
+    }
+}
 // export class using module.exports
+
+module.exports=Shape;
